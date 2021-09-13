74673e7e707be8c20937a3423498f06915484e5d
Andrea Carrero <andrea.carrero@gmail.com>
2021-09-13 16:25:04 -0500

    Tarifa DAO y VO

---
+++ src/main/java/com/amna/easyparking/dao/TarifaDAO.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.amna.easyparking.dao;
+
+import com.amna.easyparking.vo.TarifaVO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author k_bet
+ */
+public class TarifaDAO {
+    
+    private Connection con;
+    
+    public TarifaDAO (Connection con){
+        this.con = con;
+    }
+    
+    public List<TarifaVO> listar () throws SQLException {
+        String sql = "Select * from tarifa";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs =ps.executeQuery();
+        
+        List<TarifaVO> lista = new ArrayList<>();
+        while (rs.next()){
+            TarifaVO tarifaVO = new TarifaVO();
+            tarifaVO.setIdTarifa (rs.getInt("id_tarifa"));
+            tarifaVO.setValorFraccion(rs.getInt("valor_fraccion"));
+            lista.add (tarifaVO);   
+        }
+        return lista;
+        
+    }
+    
+}
